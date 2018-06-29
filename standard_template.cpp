51c87d5ad7e33fbbcf2ae1a443f5c6b14c0555bd
witaway <32620044+witaway@users.noreply.github.com>
2018-06-29 14:36:57 +0300

    Update standard_template.cpp

--- standard_template.cpp
+++ standard_template.cpp
@@ -5,6 +5,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define endl "\n"
@@ -13,7 +14,7 @@ using namespace std;
 #define ff first
 #define ss second
 #define writeln(that_arr,that_method) for(auto &that_iterator : that_arr) that_method << that_iterator << " "; that_method << endl;
-#define matrix_writeln(that_arr,that_method) for(auto &that_iterator1 : that_matrix) {for(auto &that_iterator2 : that_iterator1) that_method << that_iteartor2 << " "; that_method << endl;}
+#define matrix_writeln(that_matrix,that_method) for(auto &that_iterator1 : that_matrix) {for(auto &that_iterator2 : that_iterator1) that_method << that_iterator2 << " "; that_method << endl;}
 #define map_writeln(that_map,that_method) for(auto &that_iterator : that_map) that_method << that_iterator.ff << " : " << that_iterator.ss << endl;
 #define halt() exit(EXIT_SUCCESS)
 
@@ -25,14 +26,6 @@ typedef unsigned int ui;
 const double pi = 3.14159265358979323;
 const double eps = 0.0000001;
 
-const ll inf = 1e16;
-const int M = 1e9 + 7;
-const int N = 2e5 + 7;
-
-vector<pair<int,ll> > g[N][5];
-vector<ll> d, c;
-
-///Hi There!
 int main(int argc, char **argv) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -41,9 +34,11 @@ int main(int argc, char **argv) {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #else
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
+    freopen("file.in", "r", stdin);
+    freopen("file.out", "w", stdout);
 #endif
 
-    halt();
+
+
+    return 0;
 }
