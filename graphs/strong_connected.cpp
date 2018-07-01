daa93dcc876975469cb4e1f901109fbea47985a0
witaway <32620044+witaway@users.noreply.github.com>
2018-07-01 12:19:18 +0300

    Update strong_connected.cpp

--- graphs/strong_connected.cpp
+++ graphs/strong_connected.cpp
@@ -1,33 +1,35 @@
 int n, m;
-vector<vector<int>> g, gr;
+vector<vector<int>> g, gr; //are graph and inverted graph
 vector<bool> used;
 vector<int> order, component;
 
+//It is topological sort
 void dfs1(int v) {
 	used[v] = true;
 	for(auto to : g[v]) {
-        if(!used[to]) {
-            dfs1(to);
-        }
+        	if(!used[to]) {
+            		dfs1(to);
+        	}
 	}
 	order.push_back(v);
 }
 
+//if we can reach the edge it is part of new strong connected component
 void dfs2 (int v) {
 	used[v] = true;
 	component.push_back(v);
 	for(auto to : gr[v]) {
-        if(!used[to]) {
-            dfs2(to);
-        }
+        	if(!used[to]) {
+            		dfs2(to);
+       		}
 	}
 }
 
 int main() {
 
-    cin >> n >> m;
-    g.resize(n);
-    gr.resize(n);
+	cin >> n >> m;
+	g.resize(n);
+	gr.resize(n);
 
 	for(int i = 0; i < m; i++) {
 		int a, b;
@@ -43,7 +45,7 @@ int main() {
 		}
 	}
 
-    reverse(order.begin(), order.end());
+	reverse(order.begin(), order.end());
 	used.assign(n, false);
 	for(int i = 0; i < n; ++i) {
 		int v = order[i]
