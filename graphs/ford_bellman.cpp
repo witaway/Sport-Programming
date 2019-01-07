33d6c006013b7624c0ab9232e856fb53f1cfb6a6
witaway <32620044+witaway@users.noreply.github.com>
2019-01-07 13:07:04 +0300

    Update ford_bellman.cpp

--- graphs/ford_bellman.cpp
+++ graphs/ford_bellman.cpp
@@ -22,6 +22,9 @@ d[s] = 0; //We always can reach vertex s from vertex s.
 
 //We continue only if we relax at least one edge.
 //And we always do at least one iteration.
+
+//AND FORD-BELLMAN algorithm needs maximum only N iterations for find answer! 
+//If it is not, it mean that graph have cycle with negative weigth! BE ATTENTIVE! 
 bool any;
 do {
     any = false;
