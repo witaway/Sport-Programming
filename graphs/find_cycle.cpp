3d97ed0d2ecb7e95bd13e91c524c657383906b5e
witaway <32620044+witaway@users.noreply.github.com>
2019-01-07 15:43:31 +0300

    Update find_cycle.cpp

--- graphs/find_cycle.cpp
+++ graphs/find_cycle.cpp
@@ -1,9 +1,10 @@
-const int still_out = 0, already_out = 2, in = 1;
+const int still_out = 0, already_out = 2, in = 1; 
 int cycle_start, cycle_end;
-vector<vector<int>> g;
-vector<char> status;
-vector<int> p;
+vector<vector<int>> g; ///Adjacency list
+vector<char> status; ///Status of vertex
+vector<int> p; 
 
+///https://neerc.ifmo.ru/wiki/index.php?title=Использование_обхода_в_глубину_для_поиска_цикла
 bool dfs(int v) {
 	status[v] = in;
 	for(auto to : g[v]) {
@@ -21,33 +22,31 @@ bool dfs(int v) {
 	return false;
 }
 
-int main() {
+...
+
+p.assign(n, -1);
+status.assign(n, 0);
+
+cycle_start = -1;
+cycle_end   = -1;
+for(int i = 0; i < g.size(); ++i) {
+	if(dfs(i)) break;
+}
+
+if(cycle_start == -1) {
+	///Graph is acyclic
+} else {
+	///Graph is cyclic
 	
-	...
-
-	p.assign(n, -1);
-	status.assign(n, 0);
-	cycle_start = -1;
-	for(int i = 0; i < g.size(); ++i) {
-		if(dfs(i)) break;
+	vector<int> cycle;
+	cycle.push_back(cycle_start);
+	for (int v = cycle_end; v != cycle_start; v = p[v]) {
+		cycle.push_back (v);
 	}
+	cycle.push_back(cycle_start);
+	
+	reverse (cycle.begin(), cycle.end());
 
-	if(cycle_start == -1) {
-        cout << "Acyclic";
-    }
-	else {
-		cout << "Cyclic" << endl;
-		
-		vector<int> cycle;
-		cycle.push_back(cycle_start);
-		for (int v = cycle_end; v != cycle_start; v = p[v]) {
-			cycle.push_back (v);
-		}
-		cycle.push_back(cycle_start);
-		reverse (cycle.begin(), cycle.end());
-
-        	for(auto &x : cycle) {
-            		cout << x + 1 << " ";
-        	}
-	}
+        ///Now cycle is a list of vertexes that are in cycle
+	///WELL DONE!
 }
