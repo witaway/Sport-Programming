c6ecefee6ffd9715bfe0046684a2b079892f51c8
witaway <32620044+witaway@users.noreply.github.com>
2019-01-07 13:01:19 +0300

    Update ford_bellman.cpp

--- graphs/ford_bellman.cpp
+++ graphs/ford_bellman.cpp
@@ -1,31 +1,39 @@
+//Be attentive with size if infinity!!!
+const ll inf = 2e9+3;
+
+//Simple data structure that describes the edge of the graph
 struct edge {
-    int v, u, c;
-    edge(int v, int u, int c) {
-        this->v = v;
-        this->u = u;
-        this->c = c;
+    edge(int from, int to, int cost) {
+        this->from = from;
+        this->to = to;
+        this->cost = cost;
     }
+    int from, to;
+    int cost;
 };
 
-int main() {
-    const int inf = INT_MAX / 2;
-    vector<edge> g;
-    vector<int> d;
-    int n, m;
-    
-    ...
-    
-    d.assign(n, inf);
-    d[0] = 0;
+...
 
-    bool any = true;
-    while(any) {
-        any = false;
-        for(auto &x : g) {
-            if(d[x.u] > d[x.v] + x.c && d[x.v] < inf) {
-                d[x.u] = d[x.v] + x.c;
-                any = true;
-            }
+int n, s; //Quantity of vertexes and number of start vertex
+vector<edge> g; //List of edges
+vector<int> d(n, inf); //Shortest way between
+
+d[s] = 0; //We always can reach vertex s from vertex s.
+
+//We continue only if we relax at least one edge.
+//And we always do at least one iteration.
+bool any;
+do {
+    any = false;
+    for(auto &x : g) {
+        //We relax edge only if we can reach from-vertex!
+        if(d[x.to] > d[x.from] + x.cost && d[x.from] < inf) {
+            d[x.to] = d[x.from] + x.cost;
+            //We relaxed at least the one edge and we can continue
+            any = true;
         }
     }
-}
+} while(any);
+
+//Now d[n] is the length of the shortest way between s and n.
+//If d[n] == inf than we cant reach vertex number n
