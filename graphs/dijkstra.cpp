8b55eb7acca078ee6d7400e7bfc126632a3fbe13
witaway <32620044+witaway@users.noreply.github.com>
2019-01-07 12:15:03 +0300

    Update dijkstra.cpp

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,28 +1,27 @@
-int main() {
-    const int inf = INT_MAX / 2;
-    vector<vector<pair<int, int>>> g;
-    set<pair<int, int>> q;
-    vector<int> d(n, inf);
-    int n, s, f;
+const long long inf = 2e9+3;
 
-    ...
+int s; //start vertex
+vector<vector<pair<int, int> > > g; //adjacency list where .first is cost and .second is to-vertex
 
-    q.insert({0, s});
-    d.assign(n, inf);
-    d[s] = 0;
+//Sorted ascending queue of pair<int, int> where .first is cost and .second is to-vertex
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int> > > q;
+//Array of costs of vertexes. If vertex's cost is infinite that we cant to reach that vertex.
+vector<int> d(n, inf);
+d[s] = 0;
+q.push({0, s});
 
-    while(!q.empty()) {
-        int l  = (q.begin())->first, v = (q.begin())->second;
-        q.erase(q.begin());
-        if(l > d[v]) continue;
-        for(auto &x : g[v]) {
-            int to = x.first, len = x.second;
-            if(d[to] > d[v] + len) {
-                d[to] = d[v] + len;
-                q.insert({d[to], to});
-            }
+while(!q.empty()) {
+    int curV = q.top().ss, curW = q.top().ff;
+    q.pop();
+    //If curW is not optimal and we now have less d[curV], we continue
+    if(curW > d[curV]) continue;
+    for(auto &to : g[curV]) {
+        int toV = to.ss, toW = to.ff;
+        if(d[toV] > d[curV] + toW) {
+            d[toV] = d[curV] + toW;
+            q.push({d[toV], toV});
         }
     }
-
-    cout << (d[f] == inf ? -1 : d[f]);
 }
+
+cout << (d[f] == inf ? -1 : d[f]);
