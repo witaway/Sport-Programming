4833161a56045297cd7d3e1ef95887387ac9c1f8
witaway <32620044+witaway@users.noreply.github.com>
2019-03-30 11:33:22 +0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,53 +1,40 @@
-vector<int> a;
-int f() {
-    int e = 0;
-    for(int i = 0; i < a.size(); i++) {
-        for(int j = 0; j < i; j++) {
-            if(i == j) continue;
-            if(abs(i - j) == abs(a[i] - a[j])) e++;
-        }
-    }
-    return e;
+int n, k;
+vector < vector<int> > g;
+vector<int> mt;
+vector<char> used;
+ 
+bool try_kuhn (int v) {
+	if (used[v])  return false;
+	used[v] = true;
+	for (size_t i=0; i<g[v].size(); ++i) {
+		int to = g[v][i];
+		if (mt[to] == -1 || try_kuhn (mt[to])) {
+			mt[to] = v;
+			return true;
+		}
+	}
+	return false;
 }
 
-...
-
-srand(time(0));
-
-int n;
-cin >> n;
-a.resize(n);
-
-vector<int> ans(n);
-double T = 10, c = 0.997;
-int m = 100, e0, eans = INT_MAX;
-
-for(int i = 1; i <= n; i++) {
-    a[i - 1] = i;
+int main() {
+	... чтение графа ...
+ 
+	mt.assign (k, -1);
+	vector<char> used1 (n);
+	for (int i=0; i<n; ++i)
+		for (size_t j=0; j<g[i].size(); ++j)
+			if (mt[g[i][j]] == -1) {
+				mt[g[i][j]] = i;
+				used1[i] = true;
+				break;
+			}
+	for (int i=0; i<n; ++i) {
+		if (used1[i])  continue;
+		used.assign (n, false);
+		try_kuhn (i);
+	}
+ 
+	for (int i=0; i<k; ++i)
+		if (mt[i] != -1)
+			printf ("%d %d\n", mt[i]+1, i+1);
 }
-
-e0 = f();
-eans = INT_MAX;
-
-while(T > 1e-4) {
-    for(int iterato = 0; iterato < m; iterato++) {
-        int p1 = rand() % n, p2 = rand() % n;
-        if(p1 == p2) continue;
-        swap(a[p1], a[p2]);
-        int e1 = f();
-        int d = e1 - e0;
-        double x = exp((-1.0 * d) / T), alpha = (rand() % 1000) / 1000.0;
-        if(d >= 0 && x < alpha) {
-            swap(a[p1], a[p2]);
-        } else {
-            e0 = e1;
-        }
-        if(e0 < eans) {
-            ans = a;
-            eans = e0;
-        }
-    }
-    T *= c;
-}
-
-writeln(ans, cout);
